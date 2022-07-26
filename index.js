1ee5c18afe90c30a0ed88a68a51d14d347537db6
make789 <109366878+make789@users.noreply.github.com>
2022-07-26 13:57:53 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+function openSideMenu(){
+
+document.getElementById("sidemenu").style.width="270px";
+document.getElementById("containergrid").style.marginLeft="270px"; 
+
+}
+function closeSideMenu(){
+
+document.getElementById("sidemenu").style.width="0px";
+document.getElementById("containergrid").style.marginLeft="0px"; 
+
+}
+
+
+  
